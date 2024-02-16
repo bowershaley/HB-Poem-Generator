c70bbc4592c0378b8890820990cc9f172a3b269c
bowershaley <156343634+bowershaley@users.noreply.github.com>
2024-02-16 19:26:04 +0000

    improved the UX

--- src/index.js
+++ src/index.js
@@ -15,8 +15,12 @@ function generatePoem(event) {
   let apiKey = "b0o1e2aa04dbdcb3fatb5f7f5243c844";
   let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
   let context =
-    "you are a cyncial poem expert and like to write short poems like you are disgusted. Your mission is to generate a 4 line poem and seperate each line with a <br />. Do not use a title. Do not use italics font. Sign the poem with `- SheCodes AI` inside a <strong> elemet at the bottom on the poem on a spereate line. Make sure to follow the user instructions.";
+    "you are a cyncial poem expert and like to write short poems like you are disgusted. Your mission is to generate a 4 line poem and seperate each line with a <br />. Do not use a title. Do not use italics font. Sign the poem with `- SheCodes AI 💙` inside a <strong> elemet at the bottom on the poem on a spereate line. Make sure to follow the user instructions.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div id="blinking-text">Generating a poem for you about ${instructionsInput.value}...</div>`;
 
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
